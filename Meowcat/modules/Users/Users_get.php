a4a3040f42f2b78e2d98e7c4d979f172ea04e31b
wangziRainbow <43926608+wangziRainbow@users.noreply.github.com>
2020-11-20 09:01:34 +0800

    Add files via upload

--- Meowcat/modules/Users/Users_get.php
+++ Meowcat/modules/Users/Users_get.php
@@ -2,4 +2,22 @@
     /*
     判断登录状态：未登录显示登录注册选项，用户登陆后切换显示成昵称和注销选项
     */
+	session_start();
+	error_reporting(0);
+	
+	//包含数据库连接文件
+	include('config.php');
+	$email = $_SESSION['sign-email'];
+	$password = $_SESSION['sign-password'];
+	$user=$_SESSION['sign-user']
+
+	//检测邮箱及密码是否正确
+	$check_query = mysqli_query($config,"select user_name from users where Email='$email' and password='$password'");
+
+	if($result= mysqli_fetch_array($check_query)){
+    //登录成功
+    $_SESSION['sign-email'] = $email;
+    $_SESSION['sign-user'] = $result['user_name'];
+	$name=$result['user_name'];
+    
 ?>
