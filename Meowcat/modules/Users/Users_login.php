34d6a0781c0cb31377d1920308e9abda243f46ef
wangziRainbow <43926608+wangziRainbow@users.noreply.github.com>
2021-01-18 17:45:41 +0800

    Update Users_login.php

--- Meowcat/modules/Users/Users_login.php
+++ Meowcat/modules/Users/Users_login.php
@@ -1,29 +1,51 @@
+<html>
+<head>
+<title>登录结果</title>
+<meta charset="UTF-8">
+</head>
+
+
 <?php
     /*
-    �û����е�¼
+    用户进行登录
     */
-	header("Content-Type: text/html;charset=utf-8");
-	$email = htmlspecialchars($_POST['sign-email']);
-	$password = $_POST['sign-password'];
-
-	//�������ݿ������ļ�
-	include('config.php');
+	header("content-type:text/html;charset=utf-8");
 	
-	//������估�����Ƿ���ȷ
-	$check_query = mysqli_query($config,"select user_name from users where Email='$email' and password='$password'");
+	$email = $_POST['login-email'];
+	$password = $_POST['login-password'];
 
+	//包含数据库连接文件
+	include('../config.php');
+	
+	//检测邮箱及密码是否正确
+	$check_query = mysqli_query($config,"select user_name from users where Email='$email' and password='$password'");
+	
 	//if (!$check_query) {
 	//printf("Error: %s\n", mysqli_error($config));
 	//exit();
 	//}
 	if( $result= mysqli_fetch_array($check_query)){
-		//��¼�ɹ�
-		$_SESSION['sign-email'] = $email;
-		$_SESSION['sign-name'] = $result['user_name'];
-		$_SESSION['sign-password'] = $password;
+		//登录成功
+		$_SESSION['login-email'] = $email;
+		$_SESSION['login-name'] = $result['user_name'];
+		$_SESSION['login-duty'] = $result['duty'];
+		$_SESSION['login-password'] = $password;
 		$name=$result['user_name'];
+		$duty=$result['duty'];
+		if($duty==1)
+		{
+			header("Location: /meowcat/web/PM/tmlist.html");
+		}
+		else
+		{
+			header("Location: /meowcat/web/translator/translfile.html");
+		}
 	} 
 	else {
-		//��¼ʧ��
+		echo'<div align=center><img src="/meowcat/image/wrong.jpg" width="150px" height="100px"></div>';
+		echo'<div style="text-align:center" margin="200px">登录失败！点击此处   <a href="javascript:history.back(-1);">返 回</a></div>';
+		
+		//登录失败
 	}
 ?>
+</html>
