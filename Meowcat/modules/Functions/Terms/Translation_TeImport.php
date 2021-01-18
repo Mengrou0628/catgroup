4dade30a49089ce4a67dcd539c3a03d8d63d20bc
Mengrou0628 <61347967+Mengrou0628@users.noreply.github.com>
2021-01-19 02:38:10 +0800

    Update Translation_TeImport.php

--- Meowcat/modules/Functions/Terms/Translation_TeImport.php
+++ Meowcat/modules/Functions/Terms/Translation_TeImport.php
@@ -1,5 +1,29 @@
 <?php
-    /*
-	�����û��ϴ��������ĵ�������Ӧ���������ʾ
-	*/
+/*
+ author：王梦柔
+ */
+
+$fileInfo = $_FILES["xlsfile"];
+
+// print_r($fileInfo);
+// echo "<br>";
+
+// 获取上传文件的名称
+$fileName = $fileInfo["name"];
+
+// 获取上传文件保存的临时路径
+//$filePath = $fileInfo["tmp_name"];
+
+// echo $fileName;
+// echo "<br>";
+// echo $filePath;
+
+//移动文件
+if($fileInfo["type"]=="application/vnd.ms-excel"||$fileInfo["type"]=="application/msexcel"){
+move_uploaded_file($filePath, "../../../Resources/files/".$fileName);
+require_once('Trans_term_xlsx.php');
+}
+else{
+     echo "术语库文件格式不符合条件，请重新上传";
+}
 ?>
