4278ba07452cf50d1ac1dc9ba8088613dd7d6b9d
Mengrou0628 <61347967+Mengrou0628@users.noreply.github.com>
2021-01-19 02:14:40 +0800

    Update Translator_documents.php

--- Meowcat/modules/Translators/Translator_documents.php
+++ Meowcat/modules/Translators/Translator_documents.php
@@ -1,5 +1,20 @@
 <?php
      /*
-     ´ÓÊý¾Ý¿âÖÐÏÔÊ¾ÒëÕßÒÑ¾­·­Òë¹ýµÄÏîÄ¿ÒÔ¼°ÉÐÎ´·­ÒëµÄÏîÄ¿
+     authorï¼šçŽ‹æ¢¦æŸ”
      */
+header("content-type:application/json");
+require_once ('../config.php');
+session_start();
+$getid=$_POST['content'];
+$_SESSION["project_id"]=$getid;
+$res=mysqli_query($conn,"select file_id,filename,project_id from flproject where project_id='{$getid}'");//å–æ–‡ä»¶è¡¨é‡Œidå’Œåç§°æ•°æ®
+$arr = array();	
+if($res){
+ while($row = mysqli_fetch_array($res, MYSQLI_ASSOC)){
+  array_push($arr,$row);
+ }
+}
+//print_r($arr);
+echo json_encode($arr,JSON_UNESCAPED_UNICODE);//jsonç¼–ç 
+$conn->close();		
 ?>
