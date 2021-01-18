37054125318b3d74c4d16f4032f8a3d79f525ff5
Mengrou0628 <61347967+Mengrou0628@users.noreply.github.com>
2021-01-19 02:27:46 +0800

    Update PM_doclist.php

--- Meowcat/modules/PM/PM_project/PM_doclist.php
+++ Meowcat/modules/PM/PM_project/PM_doclist.php
@@ -7,7 +7,7 @@ require_once ('../../config.php');
 session_start();
 $project_id=$_POST["content"];
 $team_id=$_POST["content1"];
-//$res=mysqli_query($conn,"select project_id,projectname,project_id from teamproject");//取团队项目表里的全部数据
+//$res=mysqli_query($conn,"select project_id,projectname,project_id from teamproject");//测试用语句
 $res=mysqli_query($conn,"select file_id,filename, project_id,team_id from teamproject where `project_id`='$project_id' and `team_id`='$team_id'");//取个人项目表里的全部数据
 $arr = array();	
 if($res){
