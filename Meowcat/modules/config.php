e156532439e61556ce559500c5189bf60085051a
Sofiaya1726 <61358853+Sofiaya1726@users.noreply.github.com>
2021-01-19 00:13:05 +0800

    Add files via upload

--- Meowcat/modules/config.php
+++ Meowcat/modules/config.php
@@ -1,5 +1,7 @@
 <?php
-    /*
-    连接数据库
-    */
+  header('content-type:text/html;charset=utf-8');
+
+$conn=mysqli_connect('localhost','root','','cattool') or die('数据库连接出错！');
+mysqli_query($conn,'set names utf8');
+
 ?>
